ce9ca6c24fc527bc655be44bfb452c9b3860448c
victor-figueiredo <vcfo081@gmail.com>
2024-03-13 20:03:52 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      'sans': ['Poppins', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        "home": "url('https://c4.wallpaperflare.com/wallpaper/9/296/834/sexy-lingerie-wallpaper-preview.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
